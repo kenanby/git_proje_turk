3dcf54582d533c0c0d519eaa4d55b4118d370736
kenanby <67185565+kenanby@users.noreply.github.com>
2020-06-20 22:10:26 +0200

    Update FirstClass.java
    ilk komut remote repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit local repository den");
+		System.out.println("ilk commit local repository den");
 
 	}
 
